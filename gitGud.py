73fff6ece96227338a1954d8095119ac58afc49f
Antoine CHARDONNET <antoine.chardonnet@centrale-med.fr>
2025-09-18 10:57:28 +0200

    hashing objects

--- gitGud.py
+++ gitGud.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 
 
 def init(repository):
@@ -7,3 +8,31 @@ def init(repository):
     os.makedirs(os.path.join(repository, ".gitGud"))
     os.makedirs(os.path.join(repository, ".gitGud", "objects"))
     print(f"Initialized empty gitGud repository in {repository}")
+
+
+def readFile(path):
+    """Reads the contents of a file. Encoded in bytes"""
+    with open(path, "rb") as file:
+        return file.read()
+
+
+def writeFile(path, data):
+    """Write data to a file. Encoded in bytes"""
+    with open(path, "wb") as file:
+        file.write(data)
+
+
+def hashObject(data, objectType, write=True):
+    """
+    Compute SHA-1 hash of the given data and optionally write it to the object database.
+    Return SHA-1 object hash as hex string
+    """
+    header = f"{objectType} {len(data)}".encode()
+    data = header + b"\x00" + data
+    hash = hashlib.sha1(data).hexdigest()
+    if write:
+        path = os.path.join(".gitGud", "objects", hash[:2], hash[2:])
+        if not os.path.exists(path):
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            writeFile(path, data)
+    return hash
