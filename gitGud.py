7a5a06333fdb94f1c51a7cd0fd44eae19545cc94
Antoine CHARDONNET <antoine.chardonnet@centrale-med.fr>
2025-11-18 15:25:38 +0100

    Addded findObject and readObject functions

--- gitGud.py
+++ gitGud.py
@@ -37,3 +37,39 @@ def hashObject(data, objectType, write=True):
             os.makedirs(os.path.dirname(path), exist_ok=True)
             writeFile(path, zlib.compress(data))
     return hash
+
+
+def findObject(hash):
+    """
+    Find an object from it's SHA-1 hash and returns it's path.
+    Raise ValueError if not found.
+    """
+    objectDirectory = os.path.join(".gitGud", "objects", hash[:2])
+    objectName = hash[2:]
+    objects = []
+    for object in os.listdir(objectDirectory):
+        if object == objectName:
+            objects.append(object)
+    if len(objects) == 0:
+        raise ValueError(f"Object not found")
+    if len(objects) > 1:
+        raise ValueError("Multiple objects found")
+    return os.path.join(objectDirectory, objects[0])
+
+
+def readObject(hash):
+    """
+    Finds then reads an object from it's SHA-1 hash and returns it's content as a tuple : (objectType, data).
+    Raise ValueError if not found.
+    """
+    path = findObject(hash)
+    objectContent = zlib.decompress(readFile(path))
+    objectContent = objectContent.split(b"\x00")
+    header = objectContent[0]
+    objectType, size = header.decode().split(" ")
+    size = int(size)
+    data = objectContent[1]
+    assert (
+        len(data) == size
+    ), f"Data is the wrong size. Expected {size}, got {len(data)}"
+    return (objectType, data)
